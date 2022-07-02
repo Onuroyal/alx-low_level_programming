9c6ba83111308fc98a64d47d86c735e6d2521145
Onuroyal <onucheabah@gmail.com>
2022-07-02 13:05:14 -0700

    55-

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the output is a piece of aart,"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
