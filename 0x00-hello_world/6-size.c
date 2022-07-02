ec325d54ec16fe65bb6aa72648638300c161b6af
Onuroyal <onucheabah@gmail.com>
2022-07-02 14:44:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	int ainteger;
 	long along;
 	long long alonglong;
-	float float;
+	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
