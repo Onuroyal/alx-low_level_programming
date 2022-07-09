5f00596842b06e1a2ddf7b273882f9507dfb0cbd
Onuroyal <onucheabah@gmail.com>
2022-07-09 05:46:25 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints alphabets in small letters
+*
+*/
+void print_alphabet(void)
+{
+
+char alpha = 'a';
+
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
