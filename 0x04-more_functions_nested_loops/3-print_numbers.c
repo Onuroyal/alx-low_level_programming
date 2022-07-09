c0d5e3dc6737d733f7d0c04da547b7e3c783faf1
Onuroyal <onucheabah@gmail.com>
2022-07-08 22:57:44 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,12 @@
 
 void print_numbers(void)
 {
-int num;
-num = 48;
-while (num <= 57)
+char n = 0;
+
+while (n <= 9)
 {
-_putchar('0' + num);
-num++;
+_putchar('0' + n);
+n++;
 }
 _putchar('\n');
 }
