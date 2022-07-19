41bfe2451c1d4a23abb33cc5869255ddaf44c1b0
Onuroyal <onucheabah@gmail.com>
2022-07-19 00:11:39 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "main."
+
+/**
+ * print_chessboard - printsthe chessboard
+ * @a: two dimension array to print
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; i < 8; j++)
+{
+	for
