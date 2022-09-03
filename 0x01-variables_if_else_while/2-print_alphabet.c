6dcba34ca353d8219e6f7bf20c7181992b4651ea
Onuroyal <onucheabah@gmail.com>
2022-09-03 01:24:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include< stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: Away 0 (success)
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+	putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
